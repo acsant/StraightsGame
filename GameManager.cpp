d976ce07e611da70e9a878ad1355e37cc9b5fb38
Kaivalya Gandhi <kaivalya.gandhi@gmail.com>
2015-06-18 18:37:28 -0400

    edited to match game rules
    according to game manual, straights is a 4 player game

--- GameManager.cpp
+++ GameManager.cpp
@@ -51,9 +51,7 @@ void GameManager::addPlayersToGame(std::vector<std::string> type) {
 void GameManager::dealCards() {
     int num_cards = 13;
     std::vector<Card*> deck_of_cards = deck->getCards();
-    int check = players.size();
     for (int i = 0; i < players.size(); i++) {
-        check = check;
         for (int j = 0; j < num_cards; j++) {
             players[i]->addCards(deck_of_cards[i*num_cards + j]);
         }
